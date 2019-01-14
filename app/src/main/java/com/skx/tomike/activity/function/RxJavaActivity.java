6cbbf6ad5a7d639cfb4bd774f07fd3bcc7b5aa40
skxdad <244570916@qq.com>
2019-01-14 23:29:24 +0800

    TD : RxJava 版本升级

--- app/src/main/java/com/skx/tomike/activity/function/RxJavaActivity.java
+++ app/src/main/java/com/skx/tomike/activity/function/RxJavaActivity.java
@@ -6,10 +6,14 @@ import android.util.Log;
 
 import com.skx.tomike.R;
 
-import rx.Observable;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaActivity extends AppCompatActivity {
 
@@ -19,52 +23,90 @@ public class RxJavaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rx_java);
 
         // 被观察者
-        Observable.create(new Observable.OnSubscribe<String>() {
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("Hello");
-                subscriber.onNext("Hi");
-                subscriber.onNext("Aloha");
-                subscriber.onCompleted();
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                Log.e("subscribe", "1");
+                emitter.onNext("1");
+                Log.e("subscribe", "2");
+                emitter.onNext("2");
+                Log.e("subscribe", "3");
+                emitter.onNext("3");
+                emitter.onComplete();
+
+                Log.e("subscribe", "4");
+                emitter.onNext("4");
+
+                Log.e("subscribe", "5");
+                emitter.onNext("5");
+
+                Log.e("subscribe", "6");
+                emitter.onNext("6");
             }
-        }).subscribeOn(Schedulers.io())// 指定 subscribe() 所发生的线程，即 Observable.OnSubscribe 被激活时所处的线程。或者叫做事件产生的线程。
-                .observeOn(AndroidSchedulers.mainThread())// 指定 Subscriber 所运行在的线程。或者叫做事件消费的线程。
-                .subscribe(new Subscriber<String>() {
-                    @Override
-                    public void onCompleted() {
-                        Log.e("onCompleted", "完毕");
-                    }
+//        }).map(new Function<String, String>() {
+//            @Override
+//            public String apply(String s) throws Exception {
+//                return null;
+//            }
+        }).flatMap(new Function<String, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(String s) throws Exception {
+                return null;
+            }
+        })
+//                .observeOn(.mainThread())//回调在主线程
+                .subscribeOn(Schedulers.io());//执行在io线程;
 
-                    @Override
-                    public void onError(Throwable e) {
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e("onSubscribe", d.toString());
+            }
 
-                    }
+            @Override
+            public void onNext(String s) {
+                Log.e("onNext", s);
+            }
 
-                    @Override
-                    public void onNext(String s) {
-                        Log.e("onNext", s);
-                    }
+            @Override
+            public void onError(Throwable e) {
 
-                    @Override
-                    public void onStart() {
-                        super.onStart();
-                        Log.e("onAnimatorStart", "onAnimatorStart");
-                    }
-                });
+            }
 
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        // 订阅
+        observable.subscribe(observer);
+
+
+
+
+//        Observable intervalObservable = Observable.interval(3, TimeUnit.SECONDS).subscribe(new Action);
+//        Observable.range(0,5).subscribe(new Observer<Integer>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
 //
-//        Observable.just("images/logo.png") // 输入类型 String
-//                .map(new Func1<String, Bitmap>() {
-//                    @Override
-//                    public Bitmap call(String filePath) { // 参数类型 String
-//                        return getBitmapFromPath(filePath); // 返回类型 Bitmap
-//                    }
-//                })
-//                .subscribe(new Action1<Bitmap>() {
-//                    @Override
-//                    public void call(Bitmap bitmap) { // 参数类型 Bitmap
-//                        showBitmap(bitmap);
-//                    }
-//                });
+//            }
+//
+//            @Override
+//            public void onNext(Integer integer) {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        });
+
     }
 }
