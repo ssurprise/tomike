2973d8fd3fd3eee31f47fe3d518a642c072d5dd7
skxdad <244570916@qq.com>
2019-01-25 15:15:02 +0800

    TD : 添加自定义Application

---
+++ app/src/main/java/com/skx/tomike/SkxApplication.java
@@ -0,0 +1,56 @@
+package com.skx.tomike;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+import java.util.Stack;
+
+public class SkxApplication extends Application {
+
+    private static Stack<Activity> activityStack = new Stack<>();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new SwitchBackgroundCallbacks());
+    }
+
+    private class SwitchBackgroundCallbacks implements Application.ActivityLifecycleCallbacks {
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle bundle) {
+            activityStack.add(activity);
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+            activityStack.remove(activity);
+        }
+    }
+}
