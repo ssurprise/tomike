6dfe236b84e7171f1589109d40118daf7ed97e90
skxdad <244570916@qq.com>
2019-08-12 00:45:01 +0800

    TD : 更新通知demo

--- cannonlaboratory/src/main/java/com/skx/tomike/cannonlaboratory/activity/NotificationActivity.kt
+++ cannonlaboratory/src/main/java/com/skx/tomike/cannonlaboratory/activity/NotificationActivity.kt
@@ -1,19 +1,24 @@
 package com.skx.tomike.cannonlaboratory.activity
 
 import android.annotation.TargetApi
-import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import com.skx.tomike.cannonlaboratory.R
 
 
 class NotificationActivity : AppCompatActivity() {
+
+
+    private var channel_id = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,41 +30,57 @@ class NotificationActivity : AppCompatActivity() {
         val button2 = findViewById<Button>(R.id.btn_notification_cancel)
 
         // 构建通知管理器
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
 
-        button.setOnClickListener { v: View? -> createNotification(notificationManager) }
+        button.setOnClickListener { v: View? -> createNotification() }
 
-        button2.setOnClickListener { View.OnClickListener { v: View? -> cancelNotification(notificationManager) } }
+        button2.setOnClickListener { View.OnClickListener { v: View? -> cancelNotification() } }
     }
 
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    fun createNotification(notificationManager: NotificationManager) {
+    private fun createNotification() {
 
-//        val notificationCompat: NotificationCompat =
-        // 构建notification
-        val build: Notification.Builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Notification.Builder(this, "")
-        } else {
-            Notification.Builder(this)
-        }
+        val manager = NotificationManagerCompat.from(this)
+        createNotificationChannel()
 
-        val notification = build.setContentText("我是content，我是content")
-                .setContentTitle("我是title，我是title")
+        channel_id++
+
+        Log.e("channel_id", channel_id.toString())
+
+        val builder = NotificationCompat.Builder(this, "com.skx.tomike")
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .build()
-
-        var builder = NotificationCompat.Builder(this, "1")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("我是title，我是title")
-                .setContentText("我是content，我是content")
+                .setContentTitle("狗子狗子")
+                .setContentText("呼叫卢狗子，你亲爱的师狗子找你ninininininiinininininininininininnnnnnnnnn")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
 
-        notificationManager.notify(1000, notification)
+
+        manager.notify(channel_id, builder.build())
     }
 
-    fun cancelNotification(notificationManager: NotificationManager) {
-        notificationManager.cancelAll()
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "加农炮"//R.string.channel_name
+            val descriptionText = "channel_description"//  getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            // channel_id 必须一样才可以发送成功，要不然通知不生效
+            val channel = NotificationChannel(channel_id.toString(), name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun cancelNotification() {
+//        notificationManager.cancelAll()
     }
 }
