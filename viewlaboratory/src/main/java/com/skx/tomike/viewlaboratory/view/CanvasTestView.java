b08fb4488ec223f4694a749e0cff8711222c4715
skxdad <244570916@qq.com>
2019-09-17 01:24:41 +0800

    TD : 更新canvas 测试demo类

--- viewlaboratory/src/main/java/com/skx/tomike/viewlaboratory/view/CanvasTestView.java
+++ viewlaboratory/src/main/java/com/skx/tomike/viewlaboratory/view/CanvasTestView.java
@@ -1,8 +1,11 @@
 package com.skx.tomike.viewlaboratory.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -12,6 +15,14 @@ import com.skx.tomike.viewlaboratory.R;
 public class CanvasTestView extends View {
 
     private Paint mPaint;
+
+    private int mShape;
+    @ColorInt
+    private int mSolidColor;
+    @ColorInt
+    private int mStrokeColor;
+    private int mStrokeWidth;
+    private int mRadius;
 
 
     public CanvasTestView(Context context) {
@@ -24,12 +35,18 @@ public class CanvasTestView extends View {
 
     public CanvasTestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CanvasTestView);
+        mShape = array.getInt(R.styleable.CanvasTestView_shape, 0);
+        mSolidColor = array.getColor(R.styleable.CanvasTestView_solid, Color.TRANSPARENT);
+        mStrokeColor = array.getColor(R.styleable.CanvasTestView_stroke, Color.TRANSPARENT);
+        mStrokeWidth = array.getInt(R.styleable.CanvasTestView_stroke_width, -1);
+        mRadius = array.getInt(R.styleable.CanvasTestView_radius, 0);
+
+        array.recycle();
+
         mPaint = new Paint();
         mPaint.setColor(getResources().getColor(R.color.skx_3a9173));
-
-
-
-
 
     }
 
