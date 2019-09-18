59eb9767d9b49f3e96877e04bb90194243554712
skxdad <244570916@qq.com>
2019-09-18 21:16:35 +0800

    TD : 自定义view demo 更新

--- viewlaboratory/src/main/java/com/skx/tomike/viewlaboratory/view/CanvasTestView.java
+++ viewlaboratory/src/main/java/com/skx/tomike/viewlaboratory/view/CanvasTestView.java
@@ -6,15 +6,40 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.ColorInt;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.skx.tomike.viewlaboratory.R;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class CanvasTestView extends View {
 
-    private Paint mPaint;
+    /**
+     * Shape is a rectangle, possibly with rounded corners
+     */
+    public static final int RECTANGLE = 0;
+
+    /**
+     * Shape is an ellipse
+     */
+    public static final int OVAL = 1;
+
+    /**
+     * Shape is a line
+     */
+    public static final int LINE = 2;
+
+    /**
+     * Shape is a ring.
+     */
+    public static final int RING = 3;
+
+    private final Paint mFillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint mStrokePaint;   // optional, set by the caller
 
     private int mShape;
     @ColorInt
@@ -23,6 +48,11 @@ public class CanvasTestView extends View {
     private int mStrokeColor;
     private int mStrokeWidth;
     private int mRadius;
+
+    @IntDef({RECTANGLE, OVAL, LINE, RING})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Shape {
+    }
 
 
     public CanvasTestView(Context context) {
@@ -45,9 +75,10 @@ public class CanvasTestView extends View {
 
         array.recycle();
 
-        mPaint = new Paint();
-        mPaint.setColor(getResources().getColor(R.color.skx_3a9173));
-
+        mFillPaint.setColor(mSolidColor);
+        if (mStrokeWidth > 0) {
+            mStrokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        }
     }
 
     @Override
@@ -59,6 +90,6 @@ public class CanvasTestView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, 120, mPaint);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, 120, mFillPaint);
     }
 }
