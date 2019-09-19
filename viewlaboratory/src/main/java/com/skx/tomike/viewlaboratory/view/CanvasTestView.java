3ea1407054b723a33679ef4a10070cdde259e230
skxdad <244570916@qq.com>
2019-09-19 21:57:06 +0800

    TD : 自定义view demo 更新

--- viewlaboratory/src/main/java/com/skx/tomike/viewlaboratory/view/CanvasTestView.java
+++ viewlaboratory/src/main/java/com/skx/tomike/viewlaboratory/view/CanvasTestView.java
@@ -1,11 +1,10 @@
 package com.skx.tomike.viewlaboratory.view;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.support.annotation.ColorInt;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -16,40 +15,46 @@ import com.skx.tomike.viewlaboratory.R;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * 描述 : 自定义View demo类
+ * 作者 : shiguotao
+ * 版本 : V1
+ * 创建时间 : 2019-09-19 17:34
+ */
 public class CanvasTestView extends View {
 
     /**
      * Shape is a rectangle, possibly with rounded corners
      */
     public static final int RECTANGLE = 0;
+    /**
+     * Shape is a triangle.
+     */
+    public static final int TRIANGLE = 1;
 
     /**
      * Shape is an ellipse
      */
-    public static final int OVAL = 1;
-
-    /**
-     * Shape is a line
-     */
-    public static final int LINE = 2;
+    public static final int CIRCLE = 2;
 
     /**
      * Shape is a ring.
      */
     public static final int RING = 3;
 
+
     private final Paint mFillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mStrokePaint;   // optional, set by the caller
 
+    @Shape
     private int mShape;
-    @ColorInt
-    private int mSolidColor;
-    @ColorInt
-    private int mStrokeColor;
-    private int mStrokeWidth;
-    private int mRadius;
 
-    @IntDef({RECTANGLE, OVAL, LINE, RING})
+    private ColorStateList mSolidColor;
+    private ColorStateList mStrokeColor;
+    private float mStrokeWidth;
+    private float mRadius;
+
+    @IntDef({RECTANGLE, TRIANGLE, CIRCLE, RING,})
     @Retention(RetentionPolicy.SOURCE)
     public @interface Shape {
     }
@@ -67,29 +72,60 @@ public class CanvasTestView extends View {
         super(context, attrs, defStyleAttr);
 
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CanvasTestView);
+
         mShape = array.getInt(R.styleable.CanvasTestView_shape, 0);
-        mSolidColor = array.getColor(R.styleable.CanvasTestView_solid, Color.TRANSPARENT);
-        mStrokeColor = array.getColor(R.styleable.CanvasTestView_stroke, Color.TRANSPARENT);
-        mStrokeWidth = array.getInt(R.styleable.CanvasTestView_stroke_width, -1);
-        mRadius = array.getInt(R.styleable.CanvasTestView_radius, 0);
+        // 类似的可查看TextView 的颜色设置
+        mSolidColor = array.getColorStateList(R.styleable.CanvasTestView_solid_color);
+        mStrokeColor = array.getColorStateList(R.styleable.CanvasTestView_stroke_color);
+        // 参考 CircularProgressLayout.java:154
+        mStrokeWidth = array.getDimensionPixelSize(R.styleable.CanvasTestView_stroke_width, 30);
+        // 类似的可查看CardView 的 CardView_cardCornerRadius 设置
+        mRadius = array.getDimension(R.styleable.CanvasTestView_radius, 0.0F);
 
         array.recycle();
 
-        mFillPaint.setColor(mSolidColor);
-        if (mStrokeWidth > 0) {
+        // 设置填充色
+        mFillPaint.setAntiAlias(true);// 消除锯齿
+        mFillPaint.setStyle(Paint.Style.FILL);
+        mFillPaint.setColor(mSolidColor.getDefaultColor());
+
+        // 设置描边色
+
+        if (mStrokePaint == null) {
             mStrokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            mStrokePaint.setStyle(Paint.Style.STROKE);
+            mStrokePaint.setAntiAlias(true);// 消除锯齿
+            mStrokePaint.setStrokeWidth(mStrokeWidth);
+            mStrokePaint.setColor(mStrokeColor.getDefaultColor());
         }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int mode = MeasureSpec.getMode(widthMeasureSpec);
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, 120, mFillPaint);
+        switch (mShape) {
+            case RECTANGLE:
+                if (mStrokeWidth > 0 && mStrokePaint != null) {
+                    canvas.drawRect(getPaddingLeft(), getPaddingTop(), getRight() - getPaddingRight(), getBottom() - getPaddingBottom(), mStrokePaint);
+
+                } else {
+                    canvas.drawRect(getPaddingLeft(), getPaddingTop(), getWidth() - getPaddingRight(), getHeight() - getPaddingBottom(), mFillPaint);
+                }
+                break;
+            case TRIANGLE:
+                break;
+            case CIRCLE:
+                canvas.drawCircle(getWidth() >> 1, getHeight() >> 1, mRadius, mFillPaint);
+                break;
+            case RING:
+                break;
+        }
     }
 }
