391e508a721f2cb68b41367ca491d8863c55f88a
skxdad <244570916@qq.com>
2019-12-18 20:01:27 +0800

    TD : TextSwitcher demo 修改

--- app/src/main/java/com/skx/tomike/activity/TextSwitcherActivity.java
+++ app/src/main/java/com/skx/tomike/activity/TextSwitcherActivity.java
@@ -1,77 +1,70 @@
 package com.skx.tomike.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.skx.tomike.R;
-import com.skx.tomikecommonlibrary.utils.DpPxSpTool;
 
 import java.util.Random;
 
 public class TextSwitcherActivity extends Activity {
 
-    TextSwitcher textSwitcher;
-    private HorizontalScrollView pageIndicator_page;
-    private LinearLayout pageIndicator_page_container;
+    private final String NAME_COFFEE = "咖啡机";
+    private final String NAME_CAKE = "面包机";
+
+    private TextSwitcher textSwitcher;
+    private TextSwitcher textSwitcher2;
+
+    private String index = NAME_COFFEE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_switcher);
 
-        textSwitcher = (TextSwitcher) findViewById(R.id.textSwitcher);
-//        textSwitcher.setCurrentText("adv");
-//        textSwitcher.setText("123");
-
+        textSwitcher = findViewById(R.id.textSwitcher);
 
         Animation in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
         Animation out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
         textSwitcher.setInAnimation(in);
         textSwitcher.setOutAnimation(out);
 
-//        textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
-//            @Override
-//            public View makeView() {
-//                TextView textView = new TextView(TextSwitcherActivity.this);
-//                textView.setTextSize(36);
-//                return textView;
-//            }
-//        });
+        textSwitcher2 = findViewById(R.id.textSwitcher2);
+        textSwitcher2.setInAnimation(this, R.anim.show);
+        textSwitcher2.setOutAnimation(this, R.anim.hide);
+        textSwitcher2.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                TextView textView = new TextView(TextSwitcherActivity.this);
+                textView.setTextSize(20);
+                textView.setTextColor(Color.BLACK);
+
+                return textView;
+            }
+        });
 
         findViewById(R.id.textSwitcher_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textSwitcher.setText(String.valueOf(new Random().nextInt()));
+
+                if (NAME_COFFEE.equalsIgnoreCase(index)) {
+                    index = NAME_CAKE;
+
+                } else if (NAME_CAKE.equalsIgnoreCase(index)) {
+                    index = NAME_COFFEE;
+                }
+                textSwitcher2.setText(index);
             }
         });
 
-//        textSwitcher.getChildAt()
 
-
-        pageIndicator_page = (HorizontalScrollView) findViewById(R.id.pageIndicator_page);
-        pageIndicator_page_container = (LinearLayout) findViewById(R.id.pageIndicator_page_container);
-
-        addPageView(1, 5);
-    }
-
-    private void addPageView(int offset, int length) {
-        for (int i = offset; i < length; i++) {
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(DpPxSpTool.INSTANCE.dip2px(this,25), ViewGroup.LayoutParams.MATCH_PARENT);
-            TextView tv = new TextView(this);
-            tv.setTextSize(24);
-            tv.setGravity(Gravity.CENTER);
-            tv.setText(i + "");
-            tv.setLayoutParams(lp);
-            pageIndicator_page_container.addView(tv);
-        }
     }
 }
