e52f02da6934141649238225a688b57f3fd28c1e
skxdad <244570916@qq.com>
2019-12-31 20:34:22 +0800

    TD : 线程间通信 demo 修改

--- app/src/main/java/com/skx/tomike/activity/ThreadCommunicationActivity.java
+++ app/src/main/java/com/skx/tomike/activity/ThreadCommunicationActivity.java
@@ -1,10 +1,14 @@
 package com.skx.tomike.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.skx.tomike.R;
 
@@ -15,12 +19,36 @@ import com.skx.tomike.R;
  * 版本 : V1
  * 创建时间 : 2019-12-19 17:03
  */
-public class ThreadCommunicationActivity extends AppCompatActivity {
+public class ThreadCommunicationActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public final static String TAG = "ThreadCommunicationActivity";
+    private final static String TAG = "ThreadCommunicationActivity";
+
+    private final static int INIT = 20;
+
+    private ScrollView mSvLogcat;
+    private TextView mLogcat;
 
     private ProductionConsumer mProductionConsumer;
     private ProductionConsumer1 mProductionConsumer1;
+
+    private Handler mHandler = new Handler(Looper.myLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case -1:
+//                    mLogcat.append(msg.obj.toString());
+                    break;
+                case 0:
+                    mLogcat.append("\n 生产数据 " + msg.arg1);
+                    break;
+                case 1:
+                    mLogcat.append("\n 消费数据 " + msg.arg1);
+                    break;
+            }
+            mSvLogcat.fullScroll(View.FOCUS_DOWN);
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,27 +58,61 @@ public class ThreadCommunicationActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mProductionConsumer = new ProductionConsumer();
-        mProductionConsumer1 = new ProductionConsumer1();
+        mSvLogcat = findViewById(R.id.sv_threadCommunication_logcat);
+
+        findViewById(R.id.btn_threadCommunication_unused).setOnClickListener(this);
+        findViewById(R.id.btn_threadCommunication_waitAndNotify).setOnClickListener(this);
+
+        mLogcat = findViewById(R.id.tv_threadCommunication_logcat);
     }
 
-    public void onThreadCommunicationStart(View view) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    mProductionConsumer1.production();
-                }
-            }
-        }, "producer-thread").start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    mProductionConsumer1.consumer();
-                }
-            }
-        }, "consumer-thread").start();
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_threadCommunication_unused:
+                mLogcat.setText("");
+                mProductionConsumer = new ProductionConsumer();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            mProductionConsumer.production();
+                        }
+                    }
+                }, "producer-thread").start();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int i = 0;
+                        while (i < 5) {
+                            mProductionConsumer.consumer();
+                            i++;
+                        }
+                    }
+                }, "consumer-thread").start();
+                break;
+
+            case R.id.btn_threadCommunication_waitAndNotify:
+                mLogcat.setText("");
+                mProductionConsumer1 = new ProductionConsumer1();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            mProductionConsumer1.production();
+                        }
+                    }
+                }, "producer-thread").start();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            mProductionConsumer1.consumer();
+                        }
+                    }
+                }, "consumer-thread").start();
+                break;
+        }
     }
 
     class ProductionConsumer {
@@ -58,12 +120,17 @@ public class ThreadCommunicationActivity extends AppCompatActivity {
         private int i = 1;
 
         public synchronized void production() {
-            if (i <= 200)
-                Log.e(TAG, "生产数据" + i++);
+            if (i <= INIT) {
+                Message message = mHandler.obtainMessage(0);
+                message.arg1 = i++;
+                mHandler.sendMessage(message);
+            }
         }
 
         public synchronized void consumer() {
-            Log.e(TAG, "消费数据" + i);
+            Message message = mHandler.obtainMessage(1);
+            message.arg1 = i;
+            mHandler.sendMessage(message);
         }
     }
 
@@ -83,11 +150,15 @@ public class ThreadCommunicationActivity extends AppCompatActivity {
                     }
 
                 } else {
-                    if (i < 20) {
+                    if (i < INIT) {
                         i++;
-                        Log.e(TAG, "生产数据" + i);
-                        LOCK.notify();
                         isProdeced = true;
+
+                        Message message = mHandler.obtainMessage(0);
+                        message.arg1 = i;
+                        mHandler.sendMessage(message);
+
+                        LOCK.notify();
                     }
                 }
             }
@@ -96,9 +167,12 @@ public class ThreadCommunicationActivity extends AppCompatActivity {
         public synchronized void consumer() {
             synchronized (LOCK) {
                 if (isProdeced) {
-                    Log.e(TAG, "消费数据" + i);
-                    LOCK.notify();
+                    Message message = mHandler.obtainMessage(1);
+                    message.arg1 = i;
+                    mHandler.sendMessage(message);
+
                     isProdeced = false;
+                    LOCK.notify();
 
                 } else {
                     try {
