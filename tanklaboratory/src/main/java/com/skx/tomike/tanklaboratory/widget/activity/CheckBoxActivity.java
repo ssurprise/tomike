4fa2af025470da0ec0cc1b966456d2035d41788b
skxdad <244570916@qq.com>
2020-03-24 11:12:10 +0800

    TD :  CheckBox demo

--- tanklaboratory/src/main/java/com/skx/tomike/tanklaboratory/widget/activity/CheckBoxActivity.java
+++ tanklaboratory/src/main/java/com/skx/tomike/tanklaboratory/widget/activity/CheckBoxActivity.java
@@ -1,8 +1,15 @@
 package com.skx.tomike.tanklaboratory.widget.activity;
 
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import androidx.annotation.Nullable;
+
 import com.skx.tomike.tanklaboratory.R;
 import com.skx.tomikecommonlibrary.base.BaseViewModel;
 import com.skx.tomikecommonlibrary.base.SkxBaseActivity;
+import com.skx.tomikecommonlibrary.base.TitleConfig;
 
 /**
  * 描述 : CheckBox demo
@@ -18,6 +25,11 @@ public class CheckBoxActivity extends SkxBaseActivity<BaseViewModel> {
     }
 
     @Override
+    protected TitleConfig configHeaderTitle() {
+        return new TitleConfig.Builder().setTitleText("CheckBox 实验室").create();
+    }
+
+    @Override
     protected int getLayoutId() {
         return R.layout.activity_check_box;
     }
@@ -25,5 +37,20 @@ public class CheckBoxActivity extends SkxBaseActivity<BaseViewModel> {
     @Override
     protected void subscribeEvent() {
 
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CheckBox checkBox = findViewById(R.id.cb_checkBoxTest_1);
+        CheckBox checkBox2 = findViewById(R.id.cb_checkBoxTest_2);
+        CheckBox checkBox3 = findViewById(R.id.cb_checkBoxTest_3);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
     }
 }
