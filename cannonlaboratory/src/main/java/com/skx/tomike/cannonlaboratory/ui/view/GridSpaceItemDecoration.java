4cd30a73dcaa2793680d2df44adb1239597eb626
skxdad <244570916@qq.com>
2020-04-07 17:27:06 +0800

    TD :  注释修改

--- cannonlaboratory/src/main/java/com/skx/tomike/cannonlaboratory/ui/view/GridSpaceItemDecoration.java
+++ cannonlaboratory/src/main/java/com/skx/tomike/cannonlaboratory/ui/view/GridSpaceItemDecoration.java
@@ -41,8 +41,8 @@ public class GridSpaceItemDecoration extends RecyclerView.ItemDecoration {
 
     @Override
     public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
-        int position = parent.getChildAdapterPosition(view); // item 位置
-        int column = position % mSpanCount; // item 所在的列
+        int position = parent.getChildAdapterPosition(view); // 获取view 在adapter中的位置。
+        int column = position % mSpanCount; // view 所在的列
 
         outRect.left = column * mColumnSpacing / mSpanCount; // column * (列间距 * (1f / 列数))
         outRect.right = mColumnSpacing - (column + 1) * mColumnSpacing / mSpanCount; // 列间距 - (column + 1) * (列间距 * (1f /列数))
