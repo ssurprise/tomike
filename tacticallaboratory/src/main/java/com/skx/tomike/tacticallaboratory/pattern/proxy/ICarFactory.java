4960b25638afd747a487dd3d6a338e73917a2b78
skxdad <244570916@qq.com>
2020-04-14 13:32:04 +0800

    TD :  添加动态代理demo

---
+++ tacticallaboratory/src/main/java/com/skx/tomike/tacticallaboratory/pattern/proxy/ICarFactory.java
@@ -0,0 +1,16 @@
+package com.skx.tomike.tacticallaboratory.pattern.proxy;
+
+/**
+ * 描述 : 汽车工厂接口
+ * 作者 : shiguotao
+ * 版本 : V1
+ * 创建时间 : 2020/4/14 10:11 AM
+ */
+public interface ICarFactory {
+
+    /**
+     * 售卖汽车
+     */
+    void saleCar();
+
+}
