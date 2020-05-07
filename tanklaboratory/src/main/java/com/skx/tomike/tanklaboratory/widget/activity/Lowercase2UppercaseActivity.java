e742bc0f3c73055c63f989430206f8f559b2dd35
skxdad <244570916@qq.com>
2020-05-07 15:28:33 +0800

    TD :  添加EditText 小写转大写demo

--- tanklaboratory/src/main/java/com/skx/tomike/tanklaboratory/widget/activity/Lowercase2UppercaseActivity.java
+++ tanklaboratory/src/main/java/com/skx/tomike/tanklaboratory/widget/activity/Lowercase2UppercaseActivity.java
@@ -11,7 +11,8 @@ import com.skx.tomikecommonlibrary.base.SkxBaseActivity;
 import com.skx.tomikecommonlibrary.base.TitleConfig;
 
 /**
- * 描述 : EditText 小写字母转大写
+ * 描述 : EditText 小写字母转大写.
+ * http://www.huati365.com/answer/QM2W8K5WdgmRzw6
  * 作者 : shiguotao
  * 版本 : V1
  * 创建时间 : 2020/5/7 2:31 PM
