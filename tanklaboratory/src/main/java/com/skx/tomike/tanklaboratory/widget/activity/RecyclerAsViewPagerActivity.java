9c7d500e79c0862fdc96fbb47703b93764c162a2
skxdad <244570916@qq.com>
2020-06-01 14:33:14 +0800

    TD :  添加RecyclerView 仿ViewPager 切换效果demo

--- tanklaboratory/src/main/java/com/skx/tomike/tanklaboratory/widget/activity/RecyclerAsViewPagerActivity.java
+++ tanklaboratory/src/main/java/com/skx/tomike/tanklaboratory/widget/activity/RecyclerAsViewPagerActivity.java
@@ -1,10 +1,18 @@
 package com.skx.tomike.tanklaboratory.widget.activity;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.skx.tomike.tanklaboratory.R;
+import com.skx.tomike.tanklaboratory.widget.adapter.ItemAnimatorAdapter;
+import com.skx.tomike.tanklaboratory.widget.adapter.RecyclerViewBannerAdapter;
 import com.skx.tomikecommonlibrary.base.SkxBaseActivity;
+import com.skx.tomikecommonlibrary.base.TitleConfig;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 描述 : RecyclerView 仿ViewPager 实现滑页功能
@@ -14,9 +22,27 @@ import com.skx.tomikecommonlibrary.base.SkxBaseActivity;
  */
 public class RecyclerAsViewPagerActivity extends SkxBaseActivity {
 
+    private final List<String> mBannerList = new ArrayList<>();
+    private final List<String> mContentList = new LinkedList<>();
+
+
     @Override
     protected void initParams() {
+        mBannerList.add("http://pic1.win4000.com/wallpaper/6/58194994a591e.jpg");
+        mBannerList.add("http://pic1.win4000.com/wallpaper/6/5819499e74cf8.jpg");
+        mBannerList.add("http://pic1.win4000.com/wallpaper/3/584f9928e1771.jpg");
+        mBannerList.add("http://pic1.win4000.com/wallpaper/3/584f992d6bd62.jpg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590993126378&di=b2f667623306f875b48d50f120267a88&imgtype=0&src=http%3A%2F%2Fs9.rr.itc.cn%2Fr%2FwapChange%2F201611_18_11%2Fa46lpo74655993745596.gif");
+        mBannerList.add("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1761055549,119613524&fm=26&gp=0.jpg");
 
+        for (int i = 0, j = 50; i < j; i++) {
+            mContentList.add("第" + i + "个");
+        }
+    }
+
+    @Override
+    protected TitleConfig configHeaderTitle() {
+        return new TitleConfig.Builder().setTitleText("RecyclerView 仿 ViewPager效果").create();
     }
 
     @Override
@@ -26,9 +52,16 @@ public class RecyclerAsViewPagerActivity extends SkxBaseActivity {
 
     @Override
     protected void initView() {
-        RecyclerView mRvContent = findViewById(R.id.rv_recyclerviewAsViewPager_content);
-
+        RecyclerView mRvBanner = findViewById(R.id.rv_recyclerviewAsViewPager_banner);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
-        mRvContent.setLayoutManager(layoutManager);
+        mRvBanner.setLayoutManager(layoutManager);
+        mRvBanner.setAdapter(new RecyclerViewBannerAdapter(mBannerList));
+        PagerSnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(mRvBanner);
+
+
+        RecyclerView mRvContent = findViewById(R.id.rv_recyclerviewAsViewPager_content);
+        mRvContent.setLayoutManager(new LinearLayoutManager(this));
+        mRvContent.setAdapter(new ItemAnimatorAdapter(mContentList));
     }
 }
