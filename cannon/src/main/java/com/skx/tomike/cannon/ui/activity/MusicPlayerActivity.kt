78cc03e9f992cbf4b9f1cd08c0cb0420fcf4f68e
skxdad <244570916@qq.com>
2021-11-25 01:18:24 +0800

    TD : 音乐播放功能更新

--- cannon/src/main/java/com/skx/tomike/cannon/ui/activity/MusicPlayerActivity.kt
+++ cannon/src/main/java/com/skx/tomike/cannon/ui/activity/MusicPlayerActivity.kt
@@ -4,8 +4,6 @@ import android.content.ContentResolver
 import android.content.Context
 import android.database.Cursor
 import android.media.MediaPlayer
-import android.os.Handler
-import android.os.Looper
 import android.provider.MediaStore
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.skx.common.base.BaseViewModel
@@ -37,40 +35,108 @@ class MusicPlayerActivity : SkxBaseActivity<BaseViewModel>() {
     }
 
     override fun initView() {
-        val player = PlayerManagerImpl()
-        val listManager = MusicListManagerImpl(this)
-        player.init(this)
-        player.register(listManager)
-        player.play()
-
-
-        val handler = Handler(Looper.myLooper()!!)
-        handler.postDelayed({
-            player.pause()
-        }, 10000)
-        handler.postDelayed({
-            player.play()
-        }, 15000)
+        val player = PlayManagerImpl()
+//        val listManager = MusicListManagerImpl(this)
+//        player.init(this)
+//        player.play()
+//
+//
+//        val handler = Handler(Looper.myLooper()!!)
+//        handler.postDelayed({
+//            player.pause()
+//        }, 10000)
+//        handler.postDelayed({
+//            player.play()
+//        }, 15000)
 //        handler.postDelayed({
 //            player.play()
 //        }, 20000)
 
     }
+}
 
+class MusicPlayerManager {
+
+    companion object {
+        val DEFAULT_LIST_MANAGER: IMusicListManager<MusicInfo> = MusicListManagerImpl()
+    }
+
+    private var musicListManager: IMusicListManager<MusicInfo> = DEFAULT_LIST_MANAGER
+    private var playManager: IPlayManager<MusicInfo>? = null
+
+    // 表示当前播放的音乐
+    private var mIndex = -1
+
+    // 自动播放
+    private var autoplay = true
+
+
+    fun play(t: MusicInfo?, autoAddIfNotExist: Boolean = false, playNow: Boolean = false) {
+        if (t == null) {
+            return
+        }
+        // todo other check
+
+        //1.检查该音频资源是否在播放列表里
+        val indexOfKey = musicListManager.indexOfKey(t)
+
+        //2.当前播放列表里有该音频资源,直接切换至该资源index 进行播放
+        if (indexOfKey != -1) {
+            mIndex = indexOfKey
+            playManager?.play(musicListManager.get(mIndex))
+            return
+        }
+        //3.当前播放列表中无此播放资源，判断是否需要追加。
+        if (autoAddIfNotExist) {
+            musicListManager.add(t)
+        }
+        //4.是否需要立即播放新添加的音乐
+        if (autoAddIfNotExist && playNow) {
+            mIndex = musicListManager.getSize() - 1
+            justPlay(mIndex)
+        }
+    }
+
+    /**
+     * 只负责播放目标音频资源，不做任何判断处理
+     */
+    private fun justPlay(index: Int) {
+        playManager?.play(musicListManager.get(index))
+    }
+
+    /**
+     * 播放下一首
+     */
+    fun next() {
+        if (mIndex + 1 > musicListManager.getSize()) {
+            return
+        }
+        justPlay(++mIndex)
+    }
+
+    /**
+     * 播放前一首
+     */
+    fun prev() {
+        if (mIndex - 1 < 0) {
+            return
+        }
+        justPlay(--mIndex)
+    }
 }
 
 
-interface IPlayerManager<T> {
+interface IPlayManager<T> {
 
     /**
      * 注册播放列表
      */
-    fun register(list: IMusicListManager<T>)
+    fun init()
 
     /**
      * 播放
      */
-    fun play()
+    fun play(t: T?)
 
     /**
      * 暂停
@@ -78,21 +144,17 @@ interface IPlayerManager<T> {
     fun pause()
 
     /**
-     * 切换下一首音乐
+     * 当前是否正在播放中
      */
-    fun next()
+    fun isPlaying(): Boolean
 
     /**
-     * 切换上一首音乐
+     *
      */
-    fun prev()
     fun release()
 }
 
-class PlayerManagerImpl : IPlayerManager<MusicInfo> {
-
-    private var mIndex = -1
-    private var mListManager: IMusicListManager<MusicInfo>? = null
+class PlayManagerImpl : IPlayManager<MusicInfo> {
 
     private var mPlayer: MediaPlayer? = null
 
@@ -100,34 +162,35 @@ class PlayerManagerImpl : IPlayerManager<MusicInfo> {
         mPlayer = MediaPlayer.create(context, R.raw.xianer)
     }
 
-    override fun register(list: IMusicListManager<MusicInfo>) {
-        mListManager = list
+    override fun init() {
+
     }
 
-    override fun play() {
-        mPlayer?.start()
+    override fun play(t: MusicInfo?) {
+        //todo check->t
     }
 
     override fun pause() {
-        mPlayer?.pause()
-    }
 
-    override fun next() {
-    }
-
-    override fun prev() {
     }
 
     override fun release() {
         mPlayer?.stop()
         mPlayer?.release()
     }
+
+    override fun isPlaying(): Boolean {
+        return mPlayer?.isPlaying ?: false
+    }
 }
 
 
 interface IMusicListManager<T> {
-    fun register()
+
     fun add(t: T)
+
+    fun addAll(t: Collection<T>)
+
     fun remove(t: T)
 
     /**
@@ -135,51 +198,58 @@ interface IMusicListManager<T> {
      */
     fun remove(index: Int)
 
-    /**
-     * 获取下一首音乐的资源
-     */
-    fun next(): T?
+    fun getSize(): Int
+
+    fun get(index: Int): T?
 
     /**
-     * 获取上一首音乐的资源
+     * 获取该资源对应的位置
      */
-    fun prev(): T?
+    fun indexOfKey(t: T): Int
 }
 
-class MusicListManagerImpl(context: Context) : IMusicListManager<MusicInfo> {
+class MusicListManagerImpl : IMusicListManager<MusicInfo> {
 
-    private var list: MutableList<MusicInfo> = mutableListOf()
-    private val isSupportLoop = false
-    private var mIndex = -1
+    private val mList: MutableList<MusicInfo> = mutableListOf()
 
     init {
-        list = getAllMusicFile(context.applicationContext)
-    }
-
-    override fun register() {
+//        list = getAllMusicFile(context.applicationContext)
     }
 
     override fun add(t: MusicInfo) {
+        mList.add(t)
+    }
+
+    override fun addAll(t: Collection<MusicInfo>) {
+        mList.addAll(t)
     }
 
     override fun remove(t: MusicInfo) {
+        mList.remove(t)
     }
 
     override fun remove(index: Int) {
+        mList.removeAt(index)
     }
 
-    override fun next(): MusicInfo? {
-        if (mIndex + 1 in 0 until list.size) {
-            return list[++mIndex]
+    override fun get(index: Int): MusicInfo? {
+        if (index < 0 || index > mList.size) {
+            return null
         }
-        return null
+        return mList[index]
     }
 
-    override fun prev(): MusicInfo? {
-        if (mIndex - 1 in 0 until list.size) {
-            return list[--mIndex]
+    override fun getSize(): Int {
+        return mList.size
+    }
+
+    override fun indexOfKey(t: MusicInfo): Int {
+        mList.forEachIndexed { index, musicInfo ->
+            if (musicInfo.fileUrl == t.fileUrl) {
+                return index
+            }
         }
-        return null
+        return -1
     }
 
     private fun getAllMusicFile(context: Context): MutableList<MusicInfo> {
@@ -233,8 +303,6 @@ data class MusicInfo(
         val title: String? = null,
         val album: String? = null,
         val artist: String? = null,
-//        val year: String? = null,
-//        val type: String? = null,
         val size: Long = 0,
         val duration: Int = 0,
 )
