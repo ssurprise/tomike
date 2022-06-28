9a6913698811bebaa028b925d8438d9371b17c5e
guotao.shi <guotao.shi@asiainnovations.net>
2022-06-28 14:52:12 +0800

    TD : kotlin flow demo更新

--- cannon/src/main/java/com/skx/tomike/cannon/ui/activity/KotlinFlowActivity.kt
+++ cannon/src/main/java/com/skx/tomike/cannon/ui/activity/KotlinFlowActivity.kt
@@ -1,5 +1,6 @@
 package com.skx.tomike.cannon.ui.activity
 
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -9,7 +10,10 @@ import com.skx.common.base.SkxBaseActivity
 import com.skx.common.base.TitleConfig
 import com.skx.tomike.cannon.R
 import com.skx.tomike.cannon.ROUTE_PATH_FLOW
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 /**
  * 描述 : RxJava 实现并行、串行访问数据
@@ -65,11 +69,30 @@ class KotlinFlowActivity : SkxBaseActivity<BaseViewModel?>(), View.OnClickListen
     }
 
     private fun single() {
-        val flow = flow {
-
-            this.emit("1")
-            this.emit("2")
-            this.emit("3")
+        GlobalScope.launch {
+            flow {
+                Log.e(TAG, "send 1 thread:${Thread.currentThread().name}")
+                emit("1") //发送数据
+                Log.e(TAG, "send 2 thread:${Thread.currentThread().name}")
+                emit("2") //发送数据
+                Log.e(TAG, "send 3 thread:${Thread.currentThread().name}")
+                emit("3") //发送数据
+            }
+                .flowOn(Dispatchers.Main)
+                .onEmpty {
+                    Log.e(TAG, "onEmpty thread:${Thread.currentThread().name}")
+                }.onStart {
+                    Log.e(TAG, "onStart thread:${Thread.currentThread().name}")
+                }.onEach {
+                    Log.e(TAG, "onEach: $it  thread:${Thread.currentThread().name}")
+                }.onCompletion {
+                    Log.e(TAG, "onCompletion thread:${Thread.currentThread().name}")
+                }.catch { exception ->
+                    exception.message?.let { Log.e(TAG, it) }
+                }.collect {
+                    //接收数据流
+                    Log.e(TAG, "collect: $it thread:${Thread.currentThread().name}")
+                }
         }
     }
 
