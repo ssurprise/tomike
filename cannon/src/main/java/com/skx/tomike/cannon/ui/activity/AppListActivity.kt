8f93d8d469ef98f18082f91c27ac35f677789f40
skxdad <244570916@qq.com>
2023-05-06 01:11:27 +0800

    TD:更新app安装列表 demo。

--- cannon/src/main/java/com/skx/tomike/cannon/ui/activity/AppListActivity.kt
+++ cannon/src/main/java/com/skx/tomike/cannon/ui/activity/AppListActivity.kt
@@ -1,14 +1,19 @@
 package com.skx.tomike.cannon.ui.activity
 
+import android.app.Application
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.graphics.drawable.Drawable
+import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alibaba.android.arouter.facade.annotation.Route
@@ -17,6 +22,10 @@ import com.skx.common.base.SkxBaseActivity
 import com.skx.common.base.TitleConfig
 import com.skx.tomike.cannon.R
 import com.skx.tomike.cannon.ROUTE_PATH_APP_LIST
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.*
 
 
 /**
@@ -26,7 +35,10 @@ import com.skx.tomike.cannon.ROUTE_PATH_APP_LIST
  * 创建时间 : 2023/4/10 11:49 下午
  */
 @Route(path = ROUTE_PATH_APP_LIST)
-class AppListActivity : SkxBaseActivity<BaseViewModel>() {
+class AppListActivity : SkxBaseActivity<AppListViewModel>() {
+
+    private var mLoadingView: View? = null
+    private var mAdapter: AppInfoAdapter? = null
 
 
     override fun initParams() {
@@ -43,56 +55,24 @@ class AppListActivity : SkxBaseActivity<BaseViewModel>() {
     override fun initView() {
         val mRv = findViewById<RecyclerView>(R.id.rv_appList_apps)
         mRv.layoutManager = LinearLayoutManager(this)
-        val adapter = AppInfoAdapter()
-        mRv.adapter = adapter
-        val data = getAllAppInfo(this@AppListActivity)
-        adapter.setAppInfoList(data)
+        mAdapter = AppInfoAdapter()
+        mRv.adapter = mAdapter
+
+        mLoadingView = findViewById(R.id.rl_appList_loading)
+        mLoadingView?.visibility = View.VISIBLE
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewModel.getAppList(mActivity)
+    }
 
-    private fun getAllAppInfo(ctx: Context): MutableList<AppInfo> {
-        val appInfoList: MutableList<AppInfo> = mutableListOf()
-        val packageManager = ctx.packageManager
-
-        val start = System.currentTimeMillis()
-        val list = packageManager.getInstalledPackages(0)
-        val end = System.currentTimeMillis()
-        Log.e(TAG, "耗时：=${end - start}")
-        /* 注意
-        getInstalledPackages 方法并不耗时。
-        下面的两个方法比较耗时：
-        packageManager.getApplicationLabel(p.applicationInfo) - 首次获取能超过2000ms
-        p.applicationInfo.loadIcon(packageManager) - 首次获取能超过2000ms
-            2023-05-04 01:01:22.760 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=24
-            2023-05-04 01:01:25.750 6998-7033/com.skx.tomike E/HiView.HiEvent: length is 0 or exceed MAX: 1024
-            2023-05-04 01:01:25.807 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=3071
-            2023-05-04 01:01:26.335 6998-6998/com.skx.tomike E/RtgSchedManager: endActivityTransaction: margin state not match
-            2023-05-04 01:01:39.984 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=14
-            2023-05-04 01:01:40.851 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=881
-            2023-05-04 01:01:43.662 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=13
-            2023-05-04 01:01:44.502 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=853
-            2023-05-04 01:01:47.292 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=14
-            2023-05-04 01:01:48.114 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=836
-
-         */
-        for (p in list) {
-            val bean = AppInfo(0,
-                    packageManager.getApplicationLabel(p.applicationInfo).toString(),
-                    p.packageName,
-                    p.applicationInfo.loadIcon(packageManager)
-            )
-            // 判断是否是属于系统的apk
-            if (p.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0) {
-                // 系统应用
-                Log.e(TAG, "packageName=${p.packageName}, name=${bean.name}, 系统应用？yes")
-            } else {
-                Log.e(TAG, "packageName=${p.packageName}, name=${bean.name}, 系统应用? no")
-                appInfoList.add(bean)
-            }
-        }
-        val end2 = System.currentTimeMillis()
-        Log.e(TAG, "耗时2：=${end2 - start}")
-        return appInfoList
+    override fun subscribeEvent() {
+        super.subscribeEvent()
+        mViewModel.getAppsInfoLiveData().observe(this, {
+            mAdapter?.setAppInfoList(it)
+            mLoadingView?.visibility = View.GONE
+        })
     }
 
     class AppInfoAdapter : RecyclerView.Adapter<AppInfoViewHolder>() {
@@ -124,21 +104,83 @@ class AppListActivity : SkxBaseActivity<BaseViewModel>() {
 
         private val ivIcon: ImageView = itemView.findViewById(R.id.iv_appInfo_icon)
         private val tvAppName: TextView = itemView.findViewById(R.id.tv_appInfo_appName)
+        private val tvAppVersionName: TextView = itemView.findViewById(R.id.tv_appInfo_appVersionName)
 
         fun bindAppInfo(appInfo: AppInfo) {
             appInfo.icon?.run {
                 ivIcon.setImageDrawable(this)
             }
-            appInfo.name?.run {
+            appInfo.appName?.run {
                 tvAppName.text = this
+            }
+            appInfo.versionName?.run {
+                tvAppVersionName.text = this
             }
         }
     }
 
     data class AppInfo(
             var uid: Int,
-            val name: String?,
+            val appName: String?,
             val packageName: String?,
+            val versionName: String?,
             val icon: Drawable?)
 
+}
+
+class AppListViewModel(@NonNull application: Application) : BaseViewModel(application) {
+
+    private val mAppsInfoLiveData: MutableLiveData<MutableList<AppListActivity.AppInfo>> = MutableLiveData()
+
+    fun getAppsInfoLiveData(): LiveData<MutableList<AppListActivity.AppInfo>> {
+        return mAppsInfoLiveData
+    }
+
+    fun getAppList(context: Context) {
+        Observable.create<MutableList<AppListActivity.AppInfo>> { emitter ->
+
+            val packageManager = context.packageManager
+            val list = packageManager.getInstalledPackages(0)
+            /* 注意
+            getInstalledPackages 方法并不耗时。
+            下面的两个方法比较耗时：
+            packageManager.getApplicationLabel(p.applicationInfo) - 首次获取能超过2000ms
+            p.applicationInfo.loadIcon(packageManager) - 首次获取能超过2000ms
+                2023-05-04 01:01:22.760 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=24
+                2023-05-04 01:01:25.750 6998-7033/com.skx.tomike E/HiView.HiEvent: length is 0 or exceed MAX: 1024
+                2023-05-04 01:01:25.807 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=3071
+                2023-05-04 01:01:26.335 6998-6998/com.skx.tomike E/RtgSchedManager: endActivityTransaction: margin state not match
+                2023-05-04 01:01:39.984 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=14
+                2023-05-04 01:01:40.851 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=881
+                2023-05-04 01:01:43.662 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=13
+                2023-05-04 01:01:44.502 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=853
+                2023-05-04 01:01:47.292 6998-6998/com.skx.tomike E/AppListActivity: 耗时：=14
+                2023-05-04 01:01:48.114 6998-6998/com.skx.tomike E/AppListActivity: 耗时2：=836
+
+             */
+            val result: MutableList<AppListActivity.AppInfo> = mutableListOf()
+            for (p in list) {
+                val bean = AppListActivity.AppInfo(0,
+                        packageManager.getApplicationLabel(p.applicationInfo).toString(),
+                        p.packageName,
+                        p.versionName,
+                        p.applicationInfo.loadIcon(packageManager)
+                )
+                // 判断是否是属于系统的apk
+                if (p.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0) {
+                    // 系统应用
+                    Log.e("AppListActivity", "packageName=${p.packageName}, appName=${bean.appName}, 系统应用？yes")
+                } else {
+                    Log.e("AppListActivity", "packageName=${p.packageName}, appName=${bean.appName}, 系统应用? no")
+                    result.add(bean)
+                }
+            }
+            emitter?.onNext(result)
+            emitter?.onComplete()
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    mAppsInfoLiveData.postValue(it)
+                }
+    }
 }
