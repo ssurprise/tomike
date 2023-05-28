41f0a970b45cd19784535cbc85418db92ce22cdd
skxdad <244570916@qq.com>
2023-05-28 23:37:31 +0800

    TD: retrofit 新增加解密Converter

---
+++ common/src/main/java/com/skx/common/net/convert/DecryptResponseBodyConverter.java
@@ -0,0 +1,44 @@
+package com.skx.common.net.convert;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+/**
+ * 描述 : 解密转换器，用于对请求的数据先进行解密后再返回
+ * 作者 : shiguotao
+ * 版本 : V1
+ * 创建时间 : 2023/5/28 11:21 下午
+ */
+class DecryptResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+
+    private final Gson gson;
+    private final TypeAdapter<T> adapter;
+
+    DecryptResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+        this.gson = gson;
+        this.adapter = adapter;
+    }
+
+    @Override
+    public T convert(ResponseBody value) throws IOException {
+        // todo 解密操作。
+        JsonReader jsonReader = gson.newJsonReader(value.charStream());
+        try {
+            T result = adapter.read(jsonReader);
+            if (jsonReader.peek() != JsonToken.END_DOCUMENT) {
+                throw new JsonIOException("JSON document was not fully consumed.");
+            }
+            return result;
+        } finally {
+            value.close();
+        }
+    }
+}
---
+++ common/src/main/java/com/skx/common/net/convert/EncryptConvertFactory.java
@@ -0,0 +1,64 @@
+package com.skx.common.net.convert;
+
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+
+/**
+ * 描述 : 加解密 Converter
+ * 作者 : shiguotao
+ * 版本 : V1
+ * 创建时间 : 2023/5/28 11:16 下午
+ */
+class EncryptConvertFactory extends Converter.Factory {
+
+    /**
+     * Create an instance using a default {@link Gson} instance for conversion. Encoding to JSON and
+     * decoding from JSON (when no charset is specified by a header) will use UTF-8.
+     */
+    public static EncryptConvertFactory create() {
+        return create(new Gson());
+    }
+
+    /**
+     * Create an instance using {@code gson} for conversion. Encoding to JSON and
+     * decoding from JSON (when no charset is specified by a header) will use UTF-8.
+     */
+    @SuppressWarnings("ConstantConditions")
+    public static EncryptConvertFactory create(Gson gson) {
+        if (gson == null) throw new NullPointerException("gson == null");
+        return new EncryptConvertFactory(gson);
+    }
+
+
+    private final Gson gson;
+
+    private EncryptConvertFactory(Gson gson) {
+        this.gson = gson;
+    }
+
+    @Override
+    public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations,
+                                                            Retrofit retrofit) {
+        TypeAdapter<?> adapter = gson.getAdapter(TypeToken.get(type));
+        return new DecryptResponseBodyConverter<>(gson, adapter);
+    }
+
+    @Override
+    public Converter<?, RequestBody> requestBodyConverter(Type type,
+                                                          Annotation[] parameterAnnotations,
+                                                          Annotation[] methodAnnotations,
+                                                          Retrofit retrofit) {
+        TypeAdapter<?> adapter = gson.getAdapter(TypeToken.get(type));
+        return new EncryptRequestBodyConverter<>(gson, adapter);
+    }
+}
